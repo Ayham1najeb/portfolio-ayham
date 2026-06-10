a548a3f84d00f50400c97531e933362453e98755
ayham@gmail.com <152860926+Ayham1najeb@users.noreply.github.com>
2026-06-10 19:16:09 +0300

    Restore original project structure

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/portfolio-ayham/',
-  root: 'src',
-  build: {
-    outDir: '../',
-    emptyOutDir: false,
-  }
 })
